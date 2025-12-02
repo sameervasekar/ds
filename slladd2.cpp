abc09021cc015fc1077f084d4f4708f0f3d896da
sameervasekar <sameer.vasekar@gmail.com>
2025-12-03 00:57:52 +0530

    Update slladd2.cpp

--- slladd2.cpp
+++ slladd2.cpp
@@ -1,104 +1,113 @@
 // 1)	WAP to perform addition o f two polynomials using singly linked list.
 #include <iostream>
 using namespace std;
-class Node
-{
+
+class Node {
 public:
     int coeff;
     int power;
     Node *next;
 };
-Node *createNode(int c, int p)
-{
+
+Node *createNode(int c, int p) {
     Node *temp = new Node;
     temp->coeff = c;
     temp->power = p;
     temp->next = NULL;
     return temp;
 }
-void insertNode(Node *&head, int c, int p)
-{
-    if (head == NULL)
-    {
-        head = createNode(c, p);
+
+void insertOrdered(Node *&head, int c, int p) {
+    if (c == 0) return;
+    if (head == NULL || head->power < p) {
+        Node *t = createNode(c, p);
+        t->next = head;
+        head = t;
         return;
     }
-    Node *temp = head;
-    while (temp->next)
-    {
-        temp = temp->next;
+    Node *prev = NULL;
+    Node *curr = head;
+    while (curr && curr->power > p) {
+        prev = curr;
+        curr = curr->next;
     }
-    Node *newNOde = createNode(c, p);
-    temp->next = newNOde;
-
-    return;
+    if (curr && curr->power == p) {
+        curr->coeff += c;
+        if (curr->coeff == 0) {
+            if (prev) prev->next = curr->next;
+            else head = curr->next;
+            delete curr;
+        }
+    } else {
+        Node *t = createNode(c, p);
+        if (prev) {
+            prev->next = t;
+            t->next = curr;
+        } else {
+            t->next = head;
+            head = t;
+        }
+    }
 }
-Node *addPolyNomial(Node *&head_1, Node *&head_2)
-{
-    Node *temp_1 = head_1;
-    Node *temp_2 = head_2;
+
+Node *addPolynomial(Node *head1, Node *head2) {
+    Node *t1 = head1;
+    Node *t2 = head2;
     Node *result = NULL;
-
-    while (temp_1 && temp_2)
-    {
-        if (temp_1->power == temp_2->power)
-        {
-            insertNode(result, temp_1->coeff + temp_2->coeff, temp_1->power);
-            temp_2 = temp_2->next;
-            temp_1 = temp_1->next;
-        }
-        else if (temp_1->power > temp_2->power)
-        {
-            insertNode(result, temp_1->coeff, temp_1->power);
-            temp_1 = temp_1->next;
-        }
-        else if (temp_2->power > temp_1->power)
-        {
-            insertNode(result, temp_2->coeff, temp_2->power);
-            temp_2 = temp_2->next;
+    while (t1 && t2) {
+        if (t1->power == t2->power) {
+            insertOrdered(result, t1->coeff + t2->coeff, t1->power);
+            t1 = t1->next;
+            t2 = t2->next;
+        } else if (t1->power > t2->power) {
+            insertOrdered(result, t1->coeff, t1->power);
+            t1 = t1->next;
+        } else {
+            insertOrdered(result, t2->coeff, t2->power);
+            t2 = t2->next;
         }
     }
-
-    while (temp_1)
-    {
-        insertNode(result, temp_1->coeff, temp_1->power);
-        temp_1 = temp_1->next;
+    while (t1) {
+        insertOrdered(result, t1->coeff, t1->power);
+        t1 = t1->next;
     }
-
-    while (temp_2)
-    {
-        {
-            insertNode(result, temp_2->coeff, temp_2->power);
-            temp_2 = temp_2->next;
-        }
+    while (t2) {
+        insertOrdered(result, t2->coeff, t2->power);
+        t2 = t2->next;
     }
     return result;
 }
-void display(Node *&head)
-{
+
+void display(Node *head) {
     Node *temp = head;
-    while (temp)
-    {
+    if (!temp) {
+        cout << "0\n";
+        return;
+    }
+    while (temp) {
         cout << temp->coeff << "x^" << temp->power;
-        if (temp->next)
-            cout << " + ";
+        if (temp->next) cout << " + ";
         temp = temp->next;
     }
+    cout << "\n";
 }
-int main()
-{
-    Node *head_1 = createNode(3, 2);
-    insertNode(head_1, 2, 1);
-    insertNode(head_1, 3, 1);
-    insertNode(head_1, 4, 0);
-    insertNode(head_1, 5, 2);
 
-    Node *head_2 = createNode(3, 2);
-    insertNode(head_2, 2, 1);
-    insertNode(head_2, 4, 0);
+int main() {
+    Node *head_1 = NULL;
+    insertOrdered(head_1, 3, 2);
+    insertOrdered(head_1, 2, 1);
+    insertOrdered(head_1, 3, 1);
+    insertOrdered(head_1, 4, 0);
+    insertOrdered(head_1, 5, 2);
 
-    Node *head3 = addPolyNomial(head_1, head_2);
+    Node *head_2 = NULL;
+    insertOrdered(head_2, 3, 2);
+    insertOrdered(head_2, 2, 1);
+    insertOrdered(head_2, 4, 0);
+
+    Node *head3 = addPolynomial(head_1, head_2);
     display(head3);
 
     return 0;
 }
+
